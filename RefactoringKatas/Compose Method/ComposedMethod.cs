42cf57f5a9d38a16195b931b13070d94e654cdaf
Andrew <andrew.chaa@yahoo.co.uk>
2015-04-03 22:59:07 +0100

    Composed Method

---
+++ RefactoringKatas/Compose Method/ComposedMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringKatas.Compose_Method
+{
+    class ComposedMethod
+    {
+        private bool _isReadOnly;
+        private int _size;
+        private IList<object> _elements; 
+
+        public void add(object element)
+        {
+            if (_isReadOnly)
+            {
+                var newSize = _size + 1;
+                if (newSize > _elements.Count())
+                {
+                    object[] newElements = new object[_elements.Count() + 10];
+                    for (int i = 0; i < _size; i++)
+                    {
+                        newElements[i] = _elements[i];
+                    }
+                    _elements = newElements;
+                }
+                _elements[_size++] = element;
+            }
+        }
+    }
+}
---
+++ RefactoringKatas/Compose Method/ComposedMethodNew.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RefactoringKatas.Compose_Method
+{
+    class ComposedMethodNew
+    {
+        private bool _isReadOnly;
+        private int _size;
+        private IList<object> _elements; 
+
+        public void add(object element)
+        {
+            if (_isReadOnly)
+                return;
+
+            if (AtCapacity())
+                Grow();
+
+            AddElement(element);
+        }
+
+        private void AddElement(object element)
+        {
+            _elements[_size++] = element;
+        }
+
+        private bool AtCapacity()
+        {
+            return _elements.Count() < _size + 1;
+        }
+
+        private void Grow()
+        {
+            object[] newElements = new object[_elements.Count() + 10];
+            for (int i = 0; i < _size; i++)
+            {
+                newElements[i] = _elements[i];
+            }
+            _elements = newElements;
+        }
+    }
+}
